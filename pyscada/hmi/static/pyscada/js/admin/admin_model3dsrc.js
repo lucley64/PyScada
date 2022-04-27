5fa45ba4b45d3c827fa4572c7ed425d933497fa5
lucley64 <luc.leydert@gmail.com>
2022-04-27 14:48:13 +0200

    added picker in the 3d model admin

--- pyscada/hmi/static/pyscada/js/admin/admin_model3dsrc.js
+++ pyscada/hmi/static/pyscada/js/admin/admin_model3dsrc.js
@@ -1,5 +1,5 @@
 /*jshint esversion: 6 */
-import { NavCubePlugin, Viewer, XKTLoaderPlugin } from "@xeokit/xeokit-sdk";
+import { ContextMenu, NavCubePlugin, Viewer, XKTLoaderPlugin } from "@xeokit/xeokit-sdk";
 
 const viewer = new Viewer({
     canvasId: "model-canvas",
@@ -27,4 +27,88 @@ const model = loader.load({
     id: file,
     src: file,
     edges: true,
+});
+
+var lastColorized = null;
+var lastColorize = null;
+
+printDetails = (context) => {
+    if (!lastColorized || context.entity.id != lastColorized.id) {
+        if (lastColorized) {
+            lastColorized.colorize = lastColorize;
+        }
+        lastColorized = context.entity;
+        lastColorize = context.entity.colorize.slice();
+        context.entity.colorize = [0.0, 0.1, 0.0];
+    }
+    document.getElementById("model-id-container").innerHTML = "<p>Name: " + viewer.metaScene.metaObjects[context.entity.id].name + " </p><p>Id: " + context.entity.id + "</p>";
+};
+
+const menu = new ContextMenu({
+    items: [
+        [
+            {
+                title: "Get ID",
+                doAction: printDetails
+            }
+        ]
+    ]
+});
+
+viewer.cameraControl.on("doublePicked", (e) => {
+    const hit = viewer.scene.pick({
+        canvasPos: e.canvasPos,
+    });
+    const context = {
+        viewer: viewer,
+        entity: hit.entity,
+    };
+    printDetails(context);
+});
+
+viewer.cameraControl.on("rightClick", (e) => {
+    const hit = viewer.scene.pick({
+        canvasPos: e.canvasPos,
+    });
+
+    if (hit && hit.entity.isObject) {
+        menu.context = {
+            viewer: viewer,
+            entity: hit.entity,
+        };
+
+        menu.show(e.canvasPos[0], e.pagePos[1]);
+    }
+
+    e.event.preventDefault();
+});
+
+var lastEntity = null;
+
+viewer.cameraControl.on("hover", (result) => {
+    const hit = viewer.scene.pick({
+        canvasPos: result.canvasPos,
+    });
+    if (hit) {
+        if (!lastEntity || hit.entity.id != lastEntity.id) {
+            if (lastEntity) {
+                lastEntity.highlighted = false;
+            }
+            lastEntity = hit.entity;
+            hit.entity.highlighted = true;
+        }
+    }
+    else {
+        if (lastEntity) {
+            lastEntity.highlighted = false;
+            lastEntity = null;
+        }
+    }
+});
+
+viewer.cameraControl.on("hoverOff", (e) => {
+    if(lastEntity){
+        lastEntity.highlighted = false;
+        lastEntity = null;
+    }
 });
